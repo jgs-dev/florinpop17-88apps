37b19b979c1e546c0d12347036be7643e93c24c4
Juan Sierra <jgsierragr@gmail.com>
2020-08-11 12:26:38 -0500

    inputs working on the canvas

--- src/app/border-radius-previewer/border-radius-previewer.component.ts
+++ src/app/border-radius-previewer/border-radius-previewer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, Form, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-border-radius-previewer',
@@ -7,9 +8,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BorderRadiusPreviewerComponent implements OnInit {
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
-  ngOnInit(): void {
+  /**
+   * @var borderRadius object that has the border-radius variables
+   */
+  borderRadius = {
+    topLeft: 0,
+    topRight: 0,
+    bottomLeft: 0,
+    bottomRight: 0
   }
+
+  /**
+   * @var bordersForm form validator of the border inputs and values
+   */
+  bordersForm = this.formBuilder.group({
+    borderRadiusTopLeft: ["", [Validators.required, Validators.min(0), Validators.pattern(/^[0-9]+[0-9]*$/)]],
+    borderRadiusTopRight: ["", [Validators.required, Validators.min(0), Validators.pattern(/^[0-9]+[0-9]*$/)]],
+    borderRadiusBottomRight: ["", [Validators.required, Validators.min(0), Validators.pattern(/^[0-9]+[0-9]*$/)]],
+    borderRadiusBottomLeft: ["", [Validators.required, Validators.min(0), Validators.pattern(/^[0-9]+[0-9]*$/)]]
+  })
+
+  ngOnInit(): void { }
+
+  test() {
+    console.log(this.borderRadius)
+  }
+
+  getBorderRadius(): object {
+    return {"border-radius":this.borderRadius.topLeft + "px " + this.borderRadius.topRight + "px " + this.borderRadius.bottomRight + "px " + this.borderRadius.bottomLeft + "px"}
+  }
+
+  setBorderTopLeftRadius() {
+    if (this.bordersForm.get("borderRadiusTopLeft").valid) {
+      this.borderRadius.topLeft = this.bordersForm.get("borderRadiusTopLeft").value
+    }
+  }
+
+  setBorderTopRightRadius() {
+    if (this.bordersForm.get("borderRadiusTopRight").valid) {
+      this.borderRadius.topRight = this.bordersForm.get("borderRadiusTopRight").value
+    }
+  }
+
+  setBorderBottomLeftRadius() {
+    if (this.bordersForm.get("borderRadiusBottomLeft").valid) {
+      this.borderRadius.bottomLeft = this.bordersForm.get("borderRadiusBottomLeft").value
+    }
+  }
+
+  setBorderBottomRightRadius() {
+    if (this.bordersForm.get("borderRadiusBottomRight").valid) {
+      this.borderRadius.bottomRight = this.bordersForm.get("borderRadiusBottomRight").value
+    }
+  }
+
+
 
 }
