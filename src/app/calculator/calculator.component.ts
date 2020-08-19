f1f5dbf5a4bf3882de213990b98c7a8ca0646abd
Juan Sierra <jgsierragr@gmail.com>
2020-08-19 04:25:47 -0500

    basic layout of calculator, no logic

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CalculatorService } from "./../services/calculator.service"
 
 @Component({
   selector: 'app-calculator',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalculatorComponent implements OnInit {
 
-  constructor() { }
+  constructor(private calculatorService: CalculatorService) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { }
+
+  addElement(element) {
+    let display = document.querySelector("textarea")
+    display.value += element
+  }
+
+  addDecimal(){
+    let display = document.querySelector("textarea")
+    if(![...display.value].includes(",")){
+      display.value += ","
+    }
+    
   }
 
 }
